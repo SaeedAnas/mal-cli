10ef96d9f19909606a690692b027b509a9921233
SaeedAnas <saeedanas396@gmail.com>
2020-08-09 00:37:50 -0500

    Added auth struct and code challenge

--- src/auth.rs
+++ src/auth.rs
@@ -1,7 +1,10 @@
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 /// structs and methods for oauth2 authentication flow
 use serde::{Deserialize, Serialize};
 use serde_json;
 use serde_urlencoded;
+use std::iter;
 use std::str::FromStr;
 use std::time;
 
@@ -72,10 +75,82 @@ impl TokenWrapper {
     }
 }
 
+const CODE_CHALLENGE_LENGTH: usize = 128;
+
+#[derive(Clone, Serialize, Deserialize)]
+struct Auth {
+    pub client_id: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub cleint_secret: Option<String>,
+    pub redirect_url: String,
+    // pub user_agent: String,
+    pub challenge: String,
+    pub auth_code: Option<String>,
+    pub token: Option<TokenWrapper>,
+}
+
+impl Auth {
+    /// Start of a new oauth2 flow
+    /// # Parameters
+    /// * `user`
+    pub fn new<A: ToString>(
+        user_agent: A,
+        client_id: A,
+        client_secret: Option<A>,
+        redirect_url: A,
+    ) -> Self {
+        Auth {
+            client_id: client_id.to_string(),
+            cleint_secret: if let Some(cs) = client_secret {
+                Some(cs.to_string())
+            } else {
+                None
+            },
+            redirect_url: redirect_url.to_string(),
+            // user_agent: user_agent.to_string,
+            challenge: Self::new_challenge(CODE_CHALLENGE_LENGTH),
+            auth_code: None,
+            token: None,
+        }
+    }
+
+    /// Generates a new base64-encoded SHA-256 PKCE code
+    /// # Panic
+    /// `len` needs to be a value between 48 and 128
+    fn new_challenge(len: usize) -> String {
+        // Check whether the len in in between the valid length for a
+        // PKCE code (43 chars - 128 chars)
+        if len < 48 || len > 128 {
+            panic!("len is not in between 48 and 128");
+        }
+        let mut rng = thread_rng();
+        let challenge: String = iter::repeat(())
+            .map(|()| rng.sample(Alphanumeric))
+            .take(len)
+            .collect();
+        challenge
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_challenge() {
+        let challenge = Auth::new_challenge(CODE_CHALLENGE_LENGTH);
+
+        assert!(challenge.len() == CODE_CHALLENGE_LENGTH);
+        println!("{}", challenge);
+        println!(
+            "len: {}, CODE_CHALLENGE_LEN: {}",
+            challenge.len(),
+            CODE_CHALLENGE_LENGTH
+        );
+    }
+    #[test]
+    #[should_panic(expected = "len is not in between 48 and 128")]
+    fn test_challenge_len() {
+        // should panic
+        let challenge = Auth::new_challenge(5);
     }
 }
