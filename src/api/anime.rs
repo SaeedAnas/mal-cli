65ac1c8a135184848ddd4caea62c639610cb1836
SaeedAnas <saeedanas396@gmail.com>
2020-08-09 22:49:52 -0700

    added anime get endpoints

--- src/api/anime.rs
+++ src/api/anime.rs
@@ -8,7 +8,7 @@ use serde::Serialize;
 #[derive(Clone, Debug, Serialize)]
 pub struct GetAnimeListQuery {
     pub q: String,
-    pub limit: u32,
+    pub limit: u64,
     pub offset: u64,
     pub nsfw: bool,
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -21,6 +21,108 @@ pub fn get_anime_list(
 ) -> Result<PageableData<Vec<Node<Anime>>>, Error> {
     let response = get(
         &format!("{}/anime?{}", API_URL, serde_urlencoded::to_string(query)?),
+        auth,
+    )?;
+    handle_response(&response)
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct GetAnimeDetailsQuery {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub fields: Option<String>,
+    pub nsfw: bool,
+}
+
+pub fn get_anime_details(
+    anime_id: u64,
+    query: &GetAnimeDetailsQuery,
+    auth: &Auth,
+) -> Result<Anime, Error> {
+    let response = get(
+        &format!(
+            "{}/anime/{}?{}",
+            API_URL,
+            anime_id,
+            serde_urlencoded::to_string(query)?
+        ),
+        auth,
+    )?;
+    handle_response(&response)
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct GetAnimeRankingQuery {
+    pub ranking_type: AnimeRankingType,
+    pub limit: u64,
+    pub offset: u64,
+    pub nsfw: bool,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub fields: Option<String>,
+}
+
+pub fn get_anime_ranking(
+    query: &GetAnimeRankingQuery,
+    auth: &Auth,
+) -> Result<PageableData<Vec<RankingAnimePair>>, Error> {
+    let response = get(
+        &format!(
+            "{}/anime/ranking?{}",
+            API_URL,
+            serde_urlencoded::to_string(query)?
+        ),
+        auth,
+    )?;
+    handle_response(&response)
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct GetSeasonalAnimeQuery {
+    pub sort: Option<SortStyle>,
+    pub limit: u64,
+    pub offset: u64,
+    pub nsfw: bool,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub fields: Option<String>,
+}
+
+pub fn get_seasonal_anime(
+    season: &AnimeSeason,
+    query: &GetSeasonalAnimeQuery,
+    auth: &Auth,
+) -> Result<PageableData<Vec<Node<Anime>>>, Error> {
+    let season_name: &'static str = season.season.clone().into();
+    let response = get(
+        &format!(
+            "{}/anime/season/{}/{}?{}",
+            API_URL,
+            season.year,
+            season_name,
+            serde_urlencoded::to_string(query)?
+        ),
+        auth,
+    )?;
+    handle_response(&response)
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct GetSuggestedAnimeQuery {
+    pub limit: u64,
+    pub offset: u64,
+    pub nsfw: bool,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub fields: Option<String>,
+}
+
+pub fn get_suggested_anime(
+    query: &GetSuggestedAnimeQuery,
+    auth: &Auth,
+) -> Result<PageableData<Vec<Node<Anime>>>, Error> {
+    let response = get(
+        &format!(
+            "{}/anime/suggestions?{}",
+            API_URL,
+            serde_urlencoded::to_string(query)?
+        ),
         auth,
     )?;
     handle_response(&response)
