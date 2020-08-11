8e8826103e21d16528aa83fe69a02580b82bbbd9
SaeedAnas <saeedanas396@gmail.com>
2020-08-10 17:53:05 -0700

    Added user api endpoints

---
+++ src/api/user.rs
@@ -0,0 +1,27 @@
+use super::objects::*;
+use super::Error;
+use super::{delete, get, handle_response, patch, API_URL};
+use crate::auth::Auth;
+use serde::Serialize;
+
+#[derive(Clone, Debug, Serialize)]
+pub struct GetUserInformationQuery {
+    pub fields: Option<String>,
+}
+
+pub fn get_my_user_information<U: ToString>(
+    user: U,
+    query: &GetUserInformationQuery,
+    auth: &Auth,
+) -> Result<UserInfo, Error> {
+    let response = get(
+        format!(
+            "{}/users/{}?{}",
+            API_URL,
+            user.to_string(),
+            serde_urlencoded::to_string(query)?
+        ),
+        auth,
+    )?;
+    handle_response(&response)
+}
