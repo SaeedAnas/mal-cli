4666a9f96081641533d7e238b7487514910f1b42
SaeedAnas <saeedanas396@gmail.com>
2020-08-29 23:07:24 -0700

    Added cli args

--- src/config/app_config.rs
+++ src/config/app_config.rs
@@ -1,2 +1,42 @@
+use super::*;
+
 #[derive(Clone)]
-pub struct AppConfig {}
+pub struct AppConfig {
+    pub keys: KeyBindings,
+    pub theme: Theme,
+    pub behavior: BehaviorConfig,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Theme {}
+
+impl Default for Theme {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+#[derive(Clone)]
+pub struct KeyBindings {}
+
+#[derive(Clone)]
+pub struct BehaviorConfig {
+    pub seek_milliseconds: u32,
+    pub tick_rate_milliseconds: u64,
+    pub show_loading_indicator: bool,
+}
+
+// TODO: get app config from file
+impl AppConfig {
+    pub fn load() -> Result<Self, ConfigError> {
+        Ok(Self {
+            theme: Theme::default(),
+            keys: KeyBindings {},
+            behavior: BehaviorConfig {
+                seek_milliseconds: 1000,
+                tick_rate_milliseconds: 250,
+                show_loading_indicator: true,
+            },
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,98 @@ use std::io::{self, Write};
 use std::panic;
 use std::time::Duration;
 
+use clap::{App, Arg};
+
 use mal::auth::OAuth;
-
 use mal::config::{AppConfig, AuthConfig};
-
-// use mal::app::DebugInfo;
+use mal::BANNER;
 
 fn main() -> Result<()> {
-    // get opts
-    // let opts = cli::get_opts();
+    better_panic::install();
+
+    setup_panic_hook();
+
+    let args = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .before_help(BANNER)
+        .after_help(
+            "\
+        Your Config is stored in $HOME/.config/mal-cli/mal.yml\n\
+        Your MAL Client ID is stored in $HOME/.config/mal-cli/oauth2.yml\
+            ",
+        )
+        .arg(
+            Arg::with_name("search")
+                .short("s")
+                .long("search")
+                .value_name("INPUT")
+                .help("Searches for anime/manga")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("seasonal")
+                .short("S")
+                .long("seasonal")
+                .value_name("Season>,<Year")
+                .default_value("now")
+                .help("Shows Seasonal Anime")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    println!("{:?}", args);
+
+    let backend = CrosstermBackend::new(io::stdout());
+    let mut terminal = Terminal::new(backend).unwrap();
+
+    setup_terminal();
 
     // Get config
-
-    // let mut app_config = AppConfig::load()?;
+    let mut app_config = AppConfig::load()?;
 
     let mut auth_config = AuthConfig::load()?;
     let mut oauth = OAuth::get_auth(&auth_config);
 
+    // initialize app state
+
+    // start ui
+
+    cleanup_terminal();
     Ok(())
+}
+
+fn setup_terminal() {
+    let mut stdout = io::stdout();
+
+    execute!(stdout, terminal::EnterAlternateScreen).unwrap();
+    execute!(stdout, cursor::Hide).unwrap();
+
+    execute!(stdout, terminal::Clear(terminal::ClearType::All)).unwrap();
+
+    execute!(stdout, crossterm::event::EnableMouseCapture).unwrap();
+
+    terminal::enable_raw_mode().unwrap();
+}
+
+fn cleanup_terminal() {
+    let mut stdout = io::stdout();
+
+    execute!(stdout, crossterm::event::DisableMouseCapture).unwrap();
+
+    execute!(stdout, cursor::MoveTo(0, 0)).unwrap();
+    execute!(stdout, terminal::Clear(terminal::ClearType::All)).unwrap();
+
+    execute!(stdout, terminal::LeaveAlternateScreen).unwrap();
+    execute!(stdout, cursor::Show).unwrap();
+
+    terminal::disable_raw_mode().unwrap();
+}
+
+fn setup_panic_hook() {
+    panic::set_hook(Box::new(|panic_info| {
+        cleanup_terminal();
+        better_panic::Settings::auto().create_panic_handler()(panic_info);
+    }));
 }
